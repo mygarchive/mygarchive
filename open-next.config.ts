ad99646a0d00a5b6277c24f73437424a73bfa40f
hf273 <hossein.hf273@gmail.com>
2026-06-14 17:03:34 +0100

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,25 +1,10 @@
 const config = {
   default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
+    runtime: "edge",
+    placement: "global",
   },
-  edgeExternals: ["node:crypto"],
   middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
+    runtime: "edge",
   },
 };
 

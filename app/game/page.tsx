bac633c29296abeecacc07f5a5d230d48c21f031
mygarchive <hossein.hf273@gmail.com>
2026-06-16 13:17:15 +0100

    Update page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, Suspense } from 'react';
-import { useSearchParams } from 'navigation';
+import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
 function GameDetailsContent() {

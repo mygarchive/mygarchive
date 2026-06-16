500899cd633686afffd7c4c0b54fd4966bfb8d14
mygarchive <hossein.hf273@gmail.com>
2026-06-16 16:33:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  // این بخش پوشه data شما را در خروجی out کپی می‌کند تا خطای 404 برطرف شود
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const CopyPlugin = require('copy-webpack-plugin');
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [{ from: 'data', to: 'data' }],
-        })
-      );
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;

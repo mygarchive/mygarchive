3daac2c53fbe7b462f44480b42ada397fbc4e3eb
mygarchive <hossein.hf273@gmail.com>
2026-06-16 12:36:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/mygarchive',     // ✅ این خط باعث میشه گیت‌هاب مسیر استایل‌ها رو درست پیدا کنه
-  assetPrefix: '/mygarchive',  // ✅ این خط هم برای لود شدن درست فونت‌ها و فایل‌های سی‌اس‌اس هست
   images: {
     unoptimized: true,
   },
